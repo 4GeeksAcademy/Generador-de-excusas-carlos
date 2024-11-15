295bfa4e66f6968e28e54637648330ac5a407dc5
Carg21 <carlosgv512@hotmail.com>
2024-11-15 20:14:23 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+window.onload = function() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = listElements => {
+    let element = "";
+    element = listElements[Math.floor(Math.random() * listElements.length)];
+    return element;
+  };
+  getRandomElement(who);
+
+  let excuseGen = document.querySelector("#excusa");
+  console.log(excuseGen);
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what) +
+    " " +
+    getRandomElement(when);
+};
